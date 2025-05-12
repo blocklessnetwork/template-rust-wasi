294c9216510f144ad53a767364cd8ca52b6daec3
Joinhack <joinhack@gmail.com>
2025-05-12 22:37:36 +0800

    environment example.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 // Entry point to our WASI application
 
-use std::io::Write;
-
 fn main() {
-	let mut file = std::fs::OpenOptions::new()
-		.write(true)
-		.create(true)
-		.open("output.txt")
-		.expect("Failed to open file");
-	file.write_all(b"Hello World, WASI!")
-		.expect("Failed to write to file");
+	let args = std::env::vars();
+	for (key, val) in args {
+		println!("{key}={val}");
+	}
+
 }
