3f6922e3cc21763320e858f6a67635c7a276f1cc
Joinhack <joinhack@gmail.com>
2025-05-14 11:25:09 +0800

    read

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 // Entry point to our WASI application
 
-use std::io::Write;
-
 fn main() {
-	let mut file = std::fs::OpenOptions::new()
-		.write(true)
-		.create(true)
-		.open("output.txt")
-		.expect("Failed to open file");
-	file.write_all(b"Hello World, WASI!")
-		.expect("Failed to write to file");
+	let content = std::fs::read_to_string("./input.txt")
+		.expect("Failed to read file");
+	println!("File content: {content}");
 }
