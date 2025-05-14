71f42ade7c4fc739cbee4c62d033d9fd18849c4e
Joinhack <joinhack@gmail.com>
2025-05-14 14:42:05 +0800

    http

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 // Entry point to our WASI application
 
-use std::io::Write;
+use blockless_sdk::*;
 
 fn main() {
-	let mut file = std::fs::OpenOptions::new()
-		.write(true)
-		.create(true)
-		.open("output.txt")
-		.expect("Failed to open file");
-	file.write_all(b"Hello World, WASI!")
-		.expect("Failed to write to file");
+	let mut opts = HttpOptions::new("GET", 30, 10);
+    opts.headers = Some(std::collections::BTreeMap::from([(
+        "X-Test".to_string(),
+        "123".to_string(),
+    )]));
+
+    let http = BlocklessHttp::open("http://httpbin.org/anything", &opts);
+    let http = http.unwrap();
+    let body = http.get_all_body().unwrap();
+    let body = String::from_utf8(body).unwrap();
+    let bodies = match json::parse(&body).unwrap() {
+        json::JsonValue::Object(o) => o,
+        _ => panic!("must be object"),
+    };
+
+    let headers = match bodies.get("headers") {
+        Some(json::JsonValue::Object(headers)) => headers,
+        _ => panic!("must be array"),
+    };
+    headers.iter().for_each(|s| {
+        println!("{} = {}", s.0, s.1);
+    });
 }
